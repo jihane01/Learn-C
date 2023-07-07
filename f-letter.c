a42150438e314a92d5447e954e25ac827d3c2a6d
jihane01 <jihanemajd14@gmail.com>
2023-07-07 20:42:24 +0100

    add comment in file

--- f-letter.c
+++ f-letter.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/**
+ * Description-Write a C program to print a block F using the hash (#), where the F has a height of six characters and width of five and four characters.
+ */
 
 int main()
 {
